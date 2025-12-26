38f2041200b5bb25482a0c5adaad6e28c8fd78e0
suzuki3jp <suzuki3.info@gmail.com>
2025-12-26 15:20:22 +0900

    refactor: rename features/auth to features/user

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,10 +2,10 @@
 
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { googleLoginAllowedDomain } from "@/constants";
-import { firebaseClientAuth } from "@/features/auth/firebase/client";
-import { getSelf } from "@/features/auth/get-self";
-import { logIn } from "@/features/auth/log-in";
-import { logOut } from "@/features/auth/log-out";
+import { firebaseClientAuth } from "@/features/user/firebase/client";
+import { getSelf } from "@/features/user/get-self";
+import { logIn } from "@/features/user/log-in";
+import { logOut } from "@/features/user/log-out";
 
 export default function () {
   async function onLogInClick() {
