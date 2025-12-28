0d5118c7dad4c21f9abb134f78858ca380f64fbf
suzuki3jp <suzuki3.info@gmail.com>
2025-12-28 21:28:12 +0900

    chore: query の error 時の挙動を調整

--- src/query.tsx
+++ src/query.tsx
@@ -3,7 +3,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { PropsWithChildren } from "react";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 2,
+      throwOnError: true,
+    },
+  },
+});
 
 export function QueryProvider({ children }: PropsWithChildren) {
   return (
