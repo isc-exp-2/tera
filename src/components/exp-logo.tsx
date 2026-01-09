37cdb8ce86101e6000a0aa0abfe1ecff9dce3ca8
1250041 <cfao1250041@gn.iwasaki.ac.jp>
2026-01-10 01:01:36 +0900

    chore:height付けた

--- src/components/exp-logo.tsx
+++ src/components/exp-logo.tsx
@@ -3,8 +3,9 @@ import expLogo from "../../public/images/exp-logo.png";
 
 type ExpLogoProps = {
   width: number;
+  height: number;
 };
 
-export function ExpLogo({ width }: ExpLogoProps) {
-  return <Image src={expLogo} alt="Exp Logo" width={width} />;
+export function ExpLogo({ width, height }: ExpLogoProps) {
+  return <Image src={expLogo} alt="Exp Logo" width={width} height={height} />;
 }
