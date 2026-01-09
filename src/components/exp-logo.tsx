fcc7f85bab25b33905e79f14c869ed217a6c73ba
1250041 <cfao1250041@gn.iwasaki.ac.jp>
2026-01-10 02:25:40 +0900

    chore: priority追加とスプレット構文に変更

--- src/components/exp-logo.tsx
+++ src/components/exp-logo.tsx
@@ -4,8 +4,9 @@ import expLogo from "../../public/images/exp-logo.png";
 type ExpLogoProps = {
   width: number;
   height: number;
+  priority: boolean;
 };
 
-export function ExpLogo({ width, height }: ExpLogoProps) {
-  return <Image src={expLogo} alt="Exp Logo" width={width} height={height} />;
+export function ExpLogo(props: ExpLogoProps) {
+  return <Image src={expLogo} alt="Exp Logo" {...props} />;
 }
