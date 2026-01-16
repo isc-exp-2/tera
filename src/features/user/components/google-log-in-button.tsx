3da53f559f4fb3560c84d4f667b205308cb61fda
kotodamadane <ayaka.12.12@icloud.com>
2026-01-16 09:53:04 +0900

    style: 白から黒にしました

--- src/features/user/components/google-log-in-button.tsx
+++ src/features/user/components/google-log-in-button.tsx
@@ -22,7 +22,7 @@ export function GoogleLogInButton() {
     <button
       type="button"
       onClick={onClick}
-      className="w-full rounded-lg bg-cyan-400 py-2 text-white hover:bg-cyan-300"
+      className="w-full rounded-lg bg-cyan-400 py-2 text-black hover:bg-cyan-300"
     >
       Googleでログイン（gn.iwasaki.ac.jp のみ）
     </button>
