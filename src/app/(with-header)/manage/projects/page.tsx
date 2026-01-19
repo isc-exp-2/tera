6cf05d5125945231be516c37118c2c9eacaa827b
1250041 <cfao1250041@gn.iwasaki.ac.jp>
2026-01-19 09:03:31 +0900

    feat: 案件を追加するための機能を追加

--- src/app/(with-header)/manage/projects/page.tsx
+++ src/app/(with-header)/manage/projects/page.tsx
@@ -1,10 +1,11 @@
+"use client";
 import { Save } from "lucide-react";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogClose,
   DialogContent,
-  // DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -18,52 +19,118 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import {
+  ProjectExpense,
+  ProjectName,
+  ProjectStatus,
+  ProjectStatusSchema,
+} from "@/entities/project";
+import { createProject } from "@/features/project/create-project";
+import { useFormValue } from "@/hooks/useFormValue";
 
 export default function CaseManagementPage() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [projectName, setProjectName, projectNameError] = useFormValue(
+    "",
+    ProjectName,
+  );
+  const [projectExpense, setProjectExpense, projectExpenseError] = useFormValue(
+    0,
+    ProjectExpense,
+  );
+  const [status, setStatus, statusError] = useFormValue(
+    ProjectStatus.Exp,
+    ProjectStatusSchema,
+  );
+
+  const toHalfWidth = (s: string) => {
+    return s.replace(/[０-９]/g, (s) => {
+      return String.fromCharCode(s.charCodeAt(0) - 0xfee0);
+    });
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if ((e.nativeEvent as any).isComposing) return;
+
+    const normalizedValue = toHalfWidth(e.target.value);
+    setProjectExpense(Number(normalizedValue) || 0);
+  };
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    await createProject({
+      name: projectName,
+      status,
+      expense: projectExpense,
+    });
+    setIsOpen(false);
+  }
+
   return (
-    <Dialog>
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <div className="flex justify-end">
         <DialogTrigger asChild>
-          <Button className="bg-indigo-600 px-6 py-2 text-white">
+          <Button
+            className="bg-indigo-600 px-6 py-2 text-white"
+            onClick={() => {
+              setProjectName("");
+              setProjectExpense(0);
+              setStatus(ProjectStatus.Exp);
+            }}
+          >
             ＋ 案件を追加
           </Button>
         </DialogTrigger>
       </div>
-      <form>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle className="text-xl">案件を追加</DialogTitle>
-          </DialogHeader>
+
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle className="text-xl">案件を追加</DialogTitle>
+        </DialogHeader>
+        <form onSubmit={handleSubmit}>
           <div className="grid gap-4">
             <div className="grid gap-3">
               <Label className="text-gray-700">
                 案件名<span className="text-red-500">*</span>
               </Label>
-              <Input placeholder="例: コメット子安小学校" />
+              <Input
+                placeholder="例: コメット子安小学校"
+                value={projectName}
+                onChange={(e) => setProjectName(e.target.value)}
+              />
             </div>
             <div className="grid gap-3">
               <Label className="text-gray-700">
                 交通費金額（円）<span className="text-red-500">*</span>
               </Label>
-              <Input placeholder="例: 200" />
+              <Input
+                value={projectExpense === 0 ? "" : String(projectExpense)}
+                onChange={handleChange}
+                placeholder="例: 200"
+              />
             </div>
             <Label className="text-gray-700">
               案件のステータス<span className="text-red-500">*</span>
             </Label>
-            <Select>
+            <Select
+              value={status}
+              onValueChange={(v: ProjectStatus) => setStatus(v)}
+            >
               <SelectTrigger className="w-full bg-gray-100">
                 <SelectValue />
               </SelectTrigger>
               <SelectContent>
-                <SelectItem value="exp">EXP.（EXP.が費用負担）</SelectItem>
+                <SelectItem value={ProjectStatus.Exp}>
+                  EXP.（EXP.が費用負担）
+                </SelectItem>
                 <SelectItem value="external">外部（外部が費用負担）</SelectItem>
               </SelectContent>
-              <Label className="text-gray-500 text-xs">
-                ステータスは合計金額の計算に使用されます
-              </Label>
             </Select>
+            <Label className="text-gray-500 text-xs">
+              ステータスは合計金額の計算に使用されます
+            </Label>
           </div>
-          {/* <DialogFooter> */}
           <div className="flex w-full gap-4 pt-4">
             <DialogClose asChild>
               <Button variant="outline" className="flex-1 py-5">
@@ -73,14 +140,14 @@ export default function CaseManagementPage() {
             <Button
               type="submit"
               className="flex-1 bg-indigo-600 py-5 text-white"
+              disabled={!projectName || !projectExpense || !status}
             >
               <Save />
               追加
             </Button>
           </div>
-          {/* </DialogFooter> */}
-        </DialogContent>
-      </form>
+        </form>
+      </DialogContent>
     </Dialog>
   );
 }
--- src/entities/project.ts
+++ src/entities/project.ts
@@ -5,6 +5,24 @@ export enum ProjectStatus {
   External = "external",
 }
 
+export const ProjectName = v.pipe(
+  v.string(),
+  v.minLength(1, "案件名を入力してください。"),
+);
+
+export type ProjectName = v.InferOutput<typeof ProjectName>;
+
+export const ProjectExpense = v.pipe(
+  v.number(),
+  v.minValue(0, "交通費は0以上で入力してください。"),
+);
+
+export type ProjectExpense = v.InferOutput<typeof ProjectExpense>;
+
+export const ProjectStatusSchema = v.enum(ProjectStatus);
+
+export type ProjectStatusSchema = v.InferOutput<typeof ProjectStatusSchema>;
+
 export const Project = v.object({
   id: v.string(),
   name: v.pipe(v.string(), v.nonEmpty()),
