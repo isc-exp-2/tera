be1b8a93cd178f46bbac9bdfb0b49903f5847207
kotodamadane <ayaka.12.12@icloud.com>
2026-01-19 10:26:20 +0900

    feat: ログインボタンをおした後にホームにリダイレクトする

--- src/features/user/components/google-log-in-button.tsx
+++ src/features/user/components/google-log-in-button.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { Button } from "@/components/ui/button";
 import { googleLoginAllowedDomain } from "@/constants";
 import { logIn } from "@/features/user/log-in";
 import { firebaseClientAuth } from "@/firebase/client";
-import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export function GoogleLogInButton() {
+  const router = useRouter();
+
   async function onClick() {
     const googleProvider = new GoogleAuthProvider();
     googleProvider.setCustomParameters({
@@ -17,13 +20,15 @@ export function GoogleLogInButton() {
     const cred = await signInWithPopup(firebaseClientAuth, googleProvider);
     const idToken = await cred.user.getIdToken();
     await logIn(idToken);
+    router.push("/home");
   }
 
   return (
     <Button
       type="button"
       onClick={onClick}
-      className="w-full rounded-lg bg-sky-600 py-2 text-white hover:bg-sky-400 cursor-pointer">
+      className="w-full cursor-pointer rounded-lg bg-sky-600 py-2 text-white hover:bg-sky-400"
+    >
       Googleでログイン（gn.iwasaki.ac.jp のみ）
     </Button>
   );
